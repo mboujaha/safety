ddf3bee9981930b3de890098b73fdc6191270e8e
mohamed boujaha <m.boujaha@cosumar.ma>
2021-09-09 21:27:24 +0000

    added more doctypes to safety module

---
+++ safety/safety/doctype/duree_prev_d_arret_d_activite/duree_prev_d_arret_d_activite.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Cosumar Digital Factory and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Duree prev d arret d activite', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ safety/safety/doctype/inspection_categorie/inspection_categorie.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Cosumar Digital Factory and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class InspectionCategorie(Document):
+	pass
